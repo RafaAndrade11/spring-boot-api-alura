7864d7fb296382470d5602b192f3d9be84401815
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-05 05:33:39 -0300

    Commit de configurações

--- src/main/java/br/com/randrade/api/controller/MedicoController.java
+++ src/main/java/br/com/randrade/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package br.com.randrade.api.controller;
 
+import br.com.randrade.api.endereco.Endereco;
 import br.com.randrade.api.medico.DadosCadastroMedico;
+import br.com.randrade.api.medico.Medico;
+import br.com.randrade.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
