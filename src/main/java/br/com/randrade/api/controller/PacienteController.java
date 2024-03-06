ab2deaeb4aaeb0d54f768d798a355dce92398f4b
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-06 17:32:19 -0300

    Commit de alterações para salvar e validar no BDD

--- src/main/java/br/com/randrade/api/controller/PacienteController.java
+++ src/main/java/br/com/randrade/api/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package br.com.randrade.api.controller;
 
 import br.com.randrade.api.paciente.DadosCadastroPaciente;
+import br.com.randrade.api.paciente.Paciente;
+import br.com.randrade.api.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+    @Autowired
+    private PacienteRepository pacienteRepository;
 
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        pacienteRepository.save(new Paciente(dados));
     }
 }
--- src/main/java/br/com/randrade/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/br/com/randrade/api/paciente/DadosCadastroPaciente.java
@@ -1,12 +1,25 @@
 package br.com.randrade.api.paciente;
 
 import br.com.randrade.api.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroPaciente(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
         String telefone,
-        String CPF,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+        @NotNull
+        @Valid
         DadosEndereco endereco
 ) {
 }
