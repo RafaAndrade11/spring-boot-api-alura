f3ff16d5cc67f9673505b022499c5a9904f15d1a
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-09 13:52:58 -0300

    Implementação da Requisição DELETE para deixar o médico inativo

--- src/main/java/br/com/randrade/api/controller/MedicoController.java
+++ src/main/java/br/com/randrade/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package br.com.randrade.api.controller;
 
 import br.com.randrade.api.medico.*;
+import br.com.randrade.api.paciente.DadosAtualizarPaciente;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -32,6 +33,13 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizarMedicos dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 
 }
--- src/main/java/br/com/randrade/api/medico/Medico.java
+++ src/main/java/br/com/randrade/api/medico/Medico.java
@@ -23,9 +23,11 @@ public class Medico {
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+    private boolean ativo;
 
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -72,5 +74,9 @@ public class Medico {
         if (dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
