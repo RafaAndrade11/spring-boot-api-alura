32650f1c66c54228509164f7e1e144bc9f8a725e
Rafael Andrade <rafaandrade2810@gmail.com>
2024-03-15 13:57:12 -0300

    Criado o DadosDetalhesPaciente

---
+++ src/main/java/br/com/randrade/api/dtos/DadosDetalhesPaciente.java
@@ -0,0 +1,11 @@
+package br.com.randrade.api.dtos;
+
+import br.com.randrade.api.domain.Endereco;
+import br.com.randrade.api.domain.Paciente;
+
+public record DadosDetalhesPaciente(Long id, String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+    public DadosDetalhesPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
