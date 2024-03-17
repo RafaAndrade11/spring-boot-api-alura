171fde245858e64fdc7c889120acac6874cf38ed
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-03-17 03:55:44 -0300

    Criado o UsuarioRepository

---
+++ src/main/java/br/com/randrade/api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.randrade.api.repository;
+
+import br.com.randrade.api.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
