650e8e22c1ffe0abf03432cb73b11a639dbeac5e
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-03-18 11:30:31 -0300

    Commit de alteração de pasta

--- src/main/java/br/com/randrade/api/infra/exception/TratadorDeErros.java
+++ src/main/java/br/com/randrade/api/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package br.com.randrade.api.infra;
+package br.com.randrade.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
