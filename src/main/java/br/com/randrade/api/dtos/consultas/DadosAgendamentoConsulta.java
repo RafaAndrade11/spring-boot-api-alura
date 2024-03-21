0af8bd17f773e87c1ecfbade1c3a92efc5217bcc
RafaAndrade11 <rafaandrade2810@gmail.com>
2024-03-21 10:02:26 -0300

    Criação das classes e versão para Consulta

---
+++ src/main/java/br/com/randrade/api/dtos/consultas/DadosAgendamentoConsulta.java
@@ -0,0 +1,14 @@
+package br.com.randrade.api.dtos.consultas;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+        @NotNull Long idPaciente,
+        @NotNull @Future @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime data
+        ) {
+}
